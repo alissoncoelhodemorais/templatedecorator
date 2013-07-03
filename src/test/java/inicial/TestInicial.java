29c0dd0204e6fa7da28024d3c37412c3cda839ea
alissoncoelhodemorais <alisson.coelho.de.morais@gmail.com>
2013-07-03 12:07:06 -0300

    Inicio especificação dos testes.

---
+++ src/test/java/inicial/TestInicial.java
@@ -0,0 +1,21 @@
+package inicial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestInicial {
+	
+	/*
+	 * dcrt ls bens retidos e normais
+	 * iface lista de bens
+	 * quem configurarah os decoradores
+	 * ? precisa ter sobrecarga ou reaproveitamento dos bc/decoradores
+	 */
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
